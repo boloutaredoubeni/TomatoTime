518e1df3b67c7d76d47770f925eafdc54dc3da35
Boloutare Doubeni <7613722+boloutaredoubeni@users.noreply.github.com>
2017-12-05 01:27:30 -0500

    Add a respawnable, supervised client

--- src/TomatoTime/Script.fsx
+++ src/TomatoTime/Script.fsx
@@ -4,25 +4,65 @@
 // for more guidance on F# programming.
 
 #load "Library.fs"
+#r "../../packages/Colorful.Console/lib/net45/Colorful.Console.dll"
 open TomatoTime
 open System
+open System.Drawing
+open Colorful
 let num = Library.hello 42
 printfn "%i" num
 
-type TimerCommand =
-    | Start
-    | Stop
-    | Now // of AsyncReplyChannel
-
 // https://github.com/akkadotnet/akka.net/blob/dev/src/core/Akka.FSharp/FsApi.fs
 type Agent<'T> = MailboxProcessor<'T>
-let (<!) (agent: Agent<_>) message = agent .Post message
-let (<?) (agent: Agent<_>) message = agent.PostAndAsyncReply
 
-type AgentBuilder(?state) = class end
+type TimerCommand =
+  | Start
+  | Stop
+  | Now
+
+exception InvalidUserInputException of UserInput: string
+
+type ClientStatus =
+  | ClientIsAlive
+  | ClientIsDead
+  | Undetermined
+
+type AppCommand =
+  | ClientInputError of InvalidInput: string
+  | GetClientStatus of AsyncReplyChannel<ClientStatus>
+  | RestartClient of ClientConstructor:(unit -> Agent<string>) * AsyncReplyChannel<Agent<string>>
+  | FatalError of exn
+
+let supervisor exceptionHandler =
+  let agent =
+    new Agent<AppCommand>(fun inbox ->
+      do printfn "Staring the supervisor"
+      let rec loop clientStatus =
+        async {
+          let! message = inbox.Receive ()
+          match message with
+          | ClientInputError malformedInput -> 
+            do printfn "'%s' is not valid input" malformedInput
+            do! loop ClientIsDead
+          | GetClientStatus replyChannel ->
+            replyChannel .Reply clientStatus
+            do! loop clientStatus
+          | RestartClient (cons,  replyChannel) ->
+            let newClient = cons ()
+            replyChannel .Reply newClient
+            do! loop ClientIsAlive
+          | FatalError exn ->
+            do printfn "Supervisor received an exception: %A" exn
+            raise exn 
+        }
+      loop Undetermined)
+  agent .Error .Add exceptionHandler
+  agent .Start ()
+  agent
 
 let pomodoroAgent =
   Agent<TimerCommand>.Start(fun inbox ->
+    do printfn "Starting the pomodoro timer"
     let rec loop () =
       async {
         let! message = inbox.Receive ()
@@ -36,37 +76,63 @@ let pomodoroAgent =
       }
     loop ())
 
-let clientAgent =
-  Agent<string>.Start(fun inbox ->
-    let rec loop () =
-      async {
-        let! message = inbox.Receive ()
-        match message with
-        | "1" | "Start" ->
-          do pomodoroAgent <! Start
-          do! loop ()
-        | "2" | "Stop" ->
-          do pomodoroAgent <! Stop
-          do! loop ()
-        | "3" | "Now" ->
-          do pomodoroAgent <! Now
-          do! loop ()
-        | _ -> failwithf "Invalid input: %s" message
-      }
-    loop ())
+let clientAgent (supervisor: Agent<_>) =
+  let agent =
+    new Agent<string>(fun inbox ->
+      do printfn "Starting a client agent"
+      let rec loop () =
+        do printfn "Enter a command see what happens"
+        async {
+          let! message = inbox.Receive ()
+          match message with
+          | "1" | "Start" ->
+            do pomodoroAgent .Post Start
+            do! loop ()
+          | "2" | "Stop" ->
+            do pomodoroAgent .Post Stop
+            do! loop ()
+          | "3" | "Now" ->
+            do pomodoroAgent .Post Now
+            do! loop ()
+          | _ -> 
+            do
+              printfn "A client agent is stoping\n\tA new one will start soon\n\tEnter a command"
+              raise (InvalidUserInputException message)
+        }
+      loop ())
+  do
+    agent .Error .Add (function 
+      | InvalidUserInputException message -> (supervisor .Post << ClientInputError) message
+      | exn -> (supervisor .Post << FatalError) exn)
+    agent .Start ()
+  agent
 
 let main (_: array<string>) =
+  do printfn "Starting the application"
+  let clientSupervisor = supervisor raise
+  let mutable client = clientAgent clientSupervisor
   let rec readUserInput () =
-    let command = Console.ReadLine ()
-    do
-      clientAgent <! command
-      readUserInput ()
+    let clientStatusReply = 
+      (Async.RunSynchronously << clientSupervisor .PostAndTryAsyncReply) GetClientStatus
+    match clientStatusReply with
+    | None -> failwith "Supervisor timed out"
+    | Some clientStatus ->
+      if clientStatus = ClientIsDead
+        then
+          do printfn "Would you like to continue? [Yn]"
+          let input = Console .ReadLine ()
+          match input .ToUpper () with
+          | "" | "Y"  -> do client <- clientAgent clientSupervisor
+          | _ -> do printfn "Exiting..."
+      let command = Console.ReadLine ()
+      do
+        client .Post command
+        readUserInput ()
   readUserInput ()
   0
 
 do main [||] |> ignore
 
-// FIXME: restart client after failure
 // FIXME: use actual system time
 // FIXME: Use NodaTime to print time on demand
 // FIXME: Do the pomodoro
